b3c7ce0e425a887cf6aa70f2e519f9ba7af900a7
aldeko3 <aaldekoaben@gmail.com>
2024-02-22 23:05:51 +0100

    configuring astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://aldeko3.github.io'
+  site: 'https://aldeko3.github.io',
+  base: '/alpha-entrenamendu-funtzionala',
+
 });
